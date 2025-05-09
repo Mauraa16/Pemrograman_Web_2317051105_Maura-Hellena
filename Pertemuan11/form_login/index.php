eb3a8359f54350333adc64a94e2ae735b7f054eb
Mauraa16 <168169464+Mauraa16@users.noreply.github.com>
2025-05-09 19:03:01 +0700

    Update index.php

--- Pertemuan11/form_login/index.php
+++ Pertemuan11/form_login/index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+    header("Location: dashboard.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +22,12 @@ href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap
 <div class="card-body">
 <h3 class="text-center mb-4">Login</h3>
 <form method="POST" action="login.php">
+<?php
+if (isset($_SESSION['error'])) {
+echo '<div class="alert alert-danger">'.$_SESSION['error'].'</div>';
+unset($_SESSION['error']);
+}
+?>
 <div class="mb-3">
  
 <label for="username" class="form-
